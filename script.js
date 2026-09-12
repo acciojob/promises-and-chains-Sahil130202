f59bc65ad1aad0331588e4569391029364a484cb
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-12 15:17:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+
+  // Validation
+  if (age === "" || name === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+      if (Number(age) > 18) {
+        resolve(name);
+      } else {
+        reject(name);
+      }
+
+    }, 4000);
+
+  });
+
+  promise
+    .then((name) => {
+      alert(`Welcome, ${name}. You can vote.`);
+    })
+    .catch((name) => {
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
+});
